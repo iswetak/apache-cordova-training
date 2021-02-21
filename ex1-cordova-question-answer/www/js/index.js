641eab5214c2b1fb9b7fb05657fdbe31b2183074
progaurab <progaurab@gmail.com>
2021-02-21 19:03:13 +0530

    exercise update

--- ex1-cordova-question-answer/www/js/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-    alert('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-    document.getElementById('learning').innerHTML = "Instill a sense of Learning."
-}
